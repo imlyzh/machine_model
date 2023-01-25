eec2b72bfb85b04684156981133b78cf8377d241
imlyzh <enterindex@gmail.com>
2023-01-25 19:09:37 +0800

    delete interpreter struct

--- src/dfa/interpreter.rs
+++ src/dfa/interpreter.rs
@@ -1,28 +1,17 @@
 use super::*;
 use super::TargetNodeOffset::*;
 
-#[derive(Debug, Clone)]
-pub struct Interpreter<'a> {
-  pub bytecodes: &'a [Node],
-  pub pc: usize,
-  pub len: usize,
-  pub str: *const [u8],
-}
-
-// impl Interpreter<'a> {
-  // pub fn new(bcs: &'a [Node], ) {
-  // }
-// }
 
 #[inline]
-pub unsafe fn interpreter<'a>(bytecodes: &'a [Node], str: &str) -> bool {
+pub unsafe fn interpret<'a>(bytecodes: &'a [Node], str: &str) -> bool {
   // let bytes = str;
   let bytes = str.as_bytes();
-  raw_interpreter(bytecodes, bytes.as_ptr(), bytes.len())
+  raw_interpret(bytecodes, bytes.as_ptr(), bytes.len())
 }
 
+
 #[inline]
-pub unsafe fn raw_interpreter<'a>(bytecodes: &'a [Node], src: *const u8, length: usize) -> bool {
+pub unsafe fn raw_interpret<'a>(bytecodes: &'a [Node], src: *const u8, length: usize) -> bool {
   let mut pc = 0;
   let mut offset = 0;
   loop {
